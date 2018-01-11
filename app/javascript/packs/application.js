e2524db72c82e0775a65b2af289c32abaa633118
Joey Beninghove <me@joey.io>
2018-01-11 17:39:56 -0500

    Freshbarks - A Tailwind Demo
    This is a rebuild of the "Clients" page of FreshBooks using Tailwind.

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,19 @@
+/* eslint no-console:0 */
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+//
+// To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
+// layout file, like app/views/layouts/application.html.erb
+
+import Rails from "rails-ujs"
+Rails.start()
+
+import Turbolinks from "turbolinks"
+Turbolinks.start()
+
+import * as ActiveStorage from "activestorage"
+ActiveStorage.start()
+
+import "../src/application.css"
